dfffc0cebb7eab00d6ab466ddf20a758b3cafa37
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2022-10-17 00:34:16 +0600

    g

--- database/migrations/2022_10_14_221156_create_categories_table.php
+++ database/migrations/2022_10_14_221156_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('title', 100);
             $table->boolean('is_active')->defaullt(false);
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('categories')->insert([
+            'title' => 'kids',
+            'is_active' => true
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
